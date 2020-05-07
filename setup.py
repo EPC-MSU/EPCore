a80b0154065e36a0185787f1376240e05b568660
Stepan Mihalin <stopa3.10.1996@mail.ru>
2020-05-07 16:58:51 +0300

    Specify package name

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
-    version="1.0.0",
+    version="1.0.1",
     name="epcore",
-    packages=find_packages(),
+    packages=["epcore"],
     install_requires=[
         "dataclasses",  # for elements module
         "PyQt5>=5.8.2, <=5.14.0",  # for QImage and other ...
