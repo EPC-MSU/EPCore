a64e7885d7b32adccc7b4064e068cfa0702fa4f7
Stepan Mihalin <stopa3.10.1996@mail.ru>
2020-05-07 17:08:36 +0300

    Backed out changeset: 7a0eb4fb51a5

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
-    version="1.0.1",
+    version="1.0.0",
     name="epcore",
-    packages=["epcore"],
+    packages=find_packages(),
     install_requires=[
         "dataclasses",  # for elements module
         "PyQt5>=5.8.2, <=5.14.0",  # for QImage and other ...
