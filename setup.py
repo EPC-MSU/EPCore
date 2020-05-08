4fb77bc5c651fa3a579366f29666162c745628e5
Stepan Mihalin <stopa3.10.1996@mail.ru>
2020-05-08 18:59:54 +0300

    Fix typo in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     install_requires=[
         "dataclasses",  # for elements module
         "PyQt5>=5.8.2, <=5.14.0",  # for QImage and other ...
-        "jsonschema"  # for converter (utils) and format validation (different tests)
+        "jsonschema",  # for converter (utils) and format validation (different tests)
         "numpy"  # for test_iv_curve
     ],
     package_data={
