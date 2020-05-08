ba7e71ed4e6f5027a4001596507ed2848c25ec2f
Stepan Mihalin <stopa3.10.1996@mail.ru>
2020-05-08 17:12:13 +0300

    Add ivm library

--- epcore/ivmeasurer/measurerivm.py
+++ epcore/ivmeasurer/measurerivm.py
@@ -1,11 +1,50 @@
 """
 IVMeasurer Implementation for EyePoint IVM hardware measurer.
 """
-
-
+from . import IVMeasurerIdentityInformation
 from .base import IVMeasurerBase
-# import ivm <---- should be here
+from .ivm import IvmDeviceHandle
+from ..elements import IVCurve, MeasurementSettings
 
 
 class IVMeasurerIVM03(IVMeasurerBase):
-    pass
+
+    def __init__(self, url: str = ""):
+        self._device = IvmDeviceHandle(url)
+        super(IVMeasurerIVM03, self).__init__(url)
+
+    def set_settings(self, settings: MeasurementSettings):
+        device_settings = self._device.get_measurement_settings()
+        device_settings.sampling_rate = settings.sampling_rate
+        device_settings.max_voltage = settings.max_voltage
+        device_settings.probe_signal_frequency = settings.probe_signal_frequency
+        # TODO: internal_resistance, max_voltage ?
+        self._device.set_measurement_settings(device_settings)
+
+    def get_settings(self) -> MeasurementSettings:
+        device_settings = self._device.get_measurement_settings()
+        return MeasurementSettings(sampling_rate=device_settings.sampling_rate,
+                                   internal_resistance=0,  # TODO: this
+                                   max_voltage=device_settings.max_voltage,
+                                   probe_signal_frequency=device_settings.probe_signal_frequency,
+                                   precharge_delay=None)  # TODO: this
+
+    def get_identity_information(self) -> IVMeasurerIdentityInformation:
+        inf = self._device.get_identity_information()
+        return IVMeasurerIdentityInformation(manufacturer=inf.manufacturer,
+                                             device_name=inf.controller_name,  # TODO: device name?
+                                             device_class=inf.product_name,    # TODO: device class?
+                                             hardware_version=(inf.hardware_major, inf.hardware_minor,
+                                                               inf.hardware_bugfix),
+                                             firmware_version=(inf.firmware_major, inf.firmware_minor,
+                                                               inf.firmware_bugfix),
+                                             name=inf.controller_name)
+
+    def trigger_measurement(self):
+        self._device.start_measurement()
+
+    def measurement_is_ready(self) -> bool:
+        return bool(self._device.check_measurement_status().ready_status.measurement_complete)
+
+    def get_last_iv_curve(self) -> IVCurve:
+        return IVCurve()  # TODO: how to get last frame?
--- setup.py
+++ setup.py
@@ -9,5 +9,10 @@ setup(
         "dataclasses",  # for elements module
         "PyQt5>=5.8.2, <=5.14.0",  # for QImage and other ...
         "jsonschema"  # for converter (utils) and format validation (different tests)
-    ]
+    ],
+    package_data={
+        "epcore.ivmeasurer": ["ivmeasurer/ivm.dll",
+                              "ivmeasurer/ivm.lib",
+                              "ivmeasurer/libivm.so"]  # for ivmeasurer module (ivm library)
+    }
 )
