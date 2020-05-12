fbb19bcafa79026b72e4bb72878df37e3118180d
mikheev <mikheev@physlab.ru>
2020-05-12 12:42:19 +0300

    Test launcher for windows fixed: now it doesn't close the window.

--- testall.bat
+++ testall.bat
@@ -3,9 +3,9 @@ setlocal EnableDelayedExpansion
 rd /S /Q venv
 python -m venv venv
 venv\Scripts\python -m pip install -r requirements.txt
-
+echo --- Run all tests ---
 for /D %%f in (epcore\*) do (
 echo Test %%f
 venv\Scripts\python _test.py %%f
-if not !errorlevel! == 0 exit 1
 )
+pause
