da165f56b542025483541a6f3d6f3af149a4e808
mikheev <mikheev@physlab.ru>
2020-05-12 17:51:40 +0300

    Close t-38974-new_so_loading branch

--- epcore/ivmeasurer/utils.py
+++ epcore/ivmeasurer/utils.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import logging
 from ..elements import IVCurve
 from typing import List
 
 
 def plot_curves(ivcs: List[IVCurve]):
+    logging.disable(logging.WARNING)  # Matplotlib has useless output
+
     plt.figure(figsize=(12, 5))
 
     plt.subplot(1, 2, 1)
@@ -28,6 +31,8 @@ def plot_curves(ivcs: List[IVCurve]):
     plt.ylabel("Ток, мА")
 
     plt.show()
+
+    logging.disable(0)  # Enable logging back
 
 
 def plot_curve(ivc: IVCurve):
