d808dadb19dd6a3864ed8da9e0dff2098ec80911
Mikheev <ng.mikheev@ya.ru>
2020-05-14 17:18:30 +0300

    Basic product structure and config example added. Feature: #39154

---
+++ epcore/product/__init__.py
@@ -0,0 +1,14 @@
+"""
+EyePoint module, which defines product functionality:
+* number and types fo devices
+* available modes
+* labels in GUI
+"""
+from .product import MeasurementParameterOption, MeasurementParameter, ProductBase
+
+__all__ = ["MeasurementParameterOption", 
+           "MeasurementParameter",
+           "ProductBase"]
+
+__author__ = ""
+__email__ = ""
