efd654e1ced81f238b154d91a84114b521f56be6
Stepan Mihalin <stopa3.10.1996@mail.ru>
2020-05-14 21:29:19 +0300

    Add measurement plan and tests for measurement plan

--- epcore/measurementmanager/measurementplan.py
+++ epcore/measurementmanager/measurementplan.py
@@ -1,59 +1,69 @@
-from ..elements import Board, Pin, Measurement
+from ..elements import Board, Pin, Measurement, Element
 from ..ivmeasurer import IVMeasurerBase
 
+from copy import deepcopy
 
-class MeasurementPlan(Board):
+
+class MeasurementPlan:
     """
     Measurement plan (test plan) – it is a linear series of measurements.
-    It is a simple state machiine.
+    It is a simple state machine.
     All board points should be aggregated in a series.
     This series is virtual and it is not stored anywhere.
     But for every point (except the first and the last)
     there is a single next point and a single previous point.
     The initial board structure should be saved.
     """
-    def __init__(self, board: Board = Board()):
-        self.measurer = IVMeasurerBase()
-
-    def assign_measurer(self, measurer: IVMeasurerBase):
+    def __init__(self, board: Board, measurer: IVMeasurerBase):
+        self._board = deepcopy(board)
         self.measurer = measurer
+        self._all_pins = []
+        for element in board.elements:
+            for pin in element.pins:
+                self._all_pins.append(pin)
+
+        self._current_pin_index = 0
 
     def get_current_pin(self) -> Pin:
-        return Pin()
+        return self._all_pins[self._current_pin_index]
 
-    def apppend_pin(self, pin: Pin):
-        pass
+    def append_pin(self, pin: Pin):
+        self._all_pins.append(pin)
 
-    def load_next_pin(self):
-        pass
+    def go_next_pin(self):
+        self._current_pin_index += 1
+        if self._current_pin_index >= len(self._all_pins):
+            self._current_pin_index = 0
 
-    def load_prev_pin(self):
-        pass
+    def go_prev_pin(self):
+        self._current_pin_index -= 1
+        if self._current_pin_index < 0:
+            self._current_pin_index = len(self._all_pins) - 1
 
-    def load_pin(self, pin_number: int):
-        """
-        Load pin by its number in global series.
-        """
-        pass
-
-    def measure(self):
-        """
-        Make measurement for current pin.
-        You can get the result
-        by calling get_current_pin()
-        """
-
-    def assign_measurement(self, measurement: Measurement):
-        """
-        Assign measurement for current pin.
-        """
-        pass
+    def go_pin(self, pin_number: int):
+        if pin_number >= len(self._all_pins) or pin_number < 0:
+            raise ValueError(f"Pin {pin_number} does not exists")
+        self._current_pin_index = pin_number
 
     def save_last_measurement_as_reference(self):
         """
         Turn last measurement for current pin
         to a reference measurement.
         """
+        curve = self.measurer.get_last_iv_curve()
+        settings = self.measurer.get_settings()
+        measurement = Measurement(settings=settings, ivc=curve, is_reference=True)
+        pin = self.get_current_pin()
+        if not pin.measurements:
+            pin.measurements.append(measurement)
+        else:
+            # TODO: zero? Reference measure is always zero index?
+            pin.measurements[0] = measurement
 
     def export_to_board(self) -> Board:
-        return Board()
+        new_board = deepcopy(self._board)
+
+        # TODO: don't unite all pins in one element
+        element = Element(pins=self._all_pins)
+        new_board.elements = [element]
+        return new_board
---
+++ epcore/measurementmanager/tests/test_plan.py
@@ -0,0 +1,38 @@
+import unittest
+
+from epcore.ivmeasurer import IVMeasurerVirtual
+from epcore.elements import Board, Element, Pin
+from epcore.measurementmanager import MeasurementPlan
+
+
+class TestPlan(unittest.TestCase):
+    def test_plan(self):
+        pins = [Pin(0, 0, []), Pin(1, 1, [])]
+        board = Board(elements=[Element(pins=pins)])
+        measurer = IVMeasurerVirtual()
+
+        plan = MeasurementPlan(board=board, measurer=measurer)
+
+        # Check that first pin is first pin on board
+        self.assertTrue(plan.get_current_pin() == pins[0])
+
+        plan.go_next_pin()
+
+        # Check that second pin is second pin on board
+        self.assertTrue(plan.get_current_pin() == pins[1])
+
+        # Trigger measure
+        measurer.measure_iv_curve()
+        # Save last measure
+        plan.save_last_measurement_as_reference()
+
+        # Check that after measurement current pin has reference IVC
+        self.assertTrue(len(plan.get_current_pin().measurements) == 1)
+
+        # Add new pin
+        plan.append_pin(Pin(5, 5, measurements=[]))
+
+        new_board = plan.export_to_board()
+
+        # Check that here are 3 pins
+        self.assertTrue(len(new_board.elements[0].pins) == 3)
