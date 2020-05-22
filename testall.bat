e3f5ea7e287c562f8e49b68078fced5c623c9a33
Mikheev <ng.mikheev@ya.ru>
2020-05-22 10:36:44 +0300

    upgrade pip added to testall.bat.

--- testall.bat
+++ testall.bat
@@ -2,6 +2,7 @@
 setlocal EnableDelayedExpansion
 rd /S /Q venv
 python -m venv venv
+venv\Scripts\python -m pip install --upgrade pip
 venv\Scripts\python -m pip install -r requirements.txt
 echo --- Run all tests ---
 for /D %%f in (epcore\*) do (
