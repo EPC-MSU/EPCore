9689959223e73dfa0a65fb27c6afb6b0cdb6547f
Stepan Mihalin <stopa3.10.1996@mail.ru>
2020-05-31 00:04:09 +0300

    Backed out changeset: 8ac9c52d6721

--- epcore/filemanager/__init__.py
+++ epcore/filemanager/__init__.py
@@ -12,13 +12,10 @@ python -m unittest discover epcore/filemanager
 """
 
 from .ufiv import load_board_from_ufiv, save_board_to_ufiv, add_image_to_ufiv
-from .screenshot import save_screenshot
-
 
 __all__ = ["load_board_from_ufiv",
            "save_board_to_ufiv",
-           "add_image_to_ufiv",
-           "save_screenshot"]
+           "add_image_to_ufiv"]
 
 __author__ = "mihalin"
 __email__ = "mihalin@physlab.ru"
