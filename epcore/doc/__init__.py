d9e9fd1133542cf446af55d242fcc7d0bc67e4a8
Mikheev <ng.mikheev@ya.ru>
2020-06-03 18:09:10 +0300

    More flake8 fixes.

--- epcore/doc/__init__.py
+++ epcore/doc/__init__.py
@@ -7,7 +7,7 @@ _path_to_p10_elements_schema = join_path(dirname(__file__), "p10_elements.schema
 def path_to_ufiv_schema() -> str:
     return _path_to_ufiv_schema
 
-    
+
 def path_to_p10_elements_schema() -> str:
     return _path_to_p10_elements_schema
 
