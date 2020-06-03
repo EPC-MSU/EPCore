b01eef45a22a67f84318c515e3e9515bd61c28d7
Mikheev <ng.mikheev@ya.ru>
2020-06-03 17:30:02 +0300

    EP P10 elements.json schema added.

--- epcore/doc/__init__.py
+++ epcore/doc/__init__.py
@@ -1,10 +1,14 @@
 from os.path import dirname, join as join_path
 
 _path_to_ufiv_schema = join_path(dirname(__file__), "ufiv.schema.json")
+_path_to_p10_elements_schema = join_path(dirname(__file__), "p10_elements.schema.json")
 
 
 def path_to_ufiv_schema() -> str:
     return _path_to_ufiv_schema
 
+def path_to_p10_elements_schema() -> str:
+    return _path_to_p10_elements_schema
 
-__all__ = ["path_to_ufiv_schema"]
+__all__ = ["path_to_ufiv_schema",
+           "path_to_p10_elements_schema"]
