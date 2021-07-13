8f3e545db8e66fef40d80c2c253b1edaa6275bed
Vasily <devnull@localhost>
2021-07-13 19:37:59 +0400

    update example

--- epcore/ivmeasurer/__main__.py
+++ epcore/ivmeasurer/__main__.py
@@ -1,5 +1,6 @@
 import logging
 import argparse
+from .safe_opener import BadFirmwareVersion, BadConfig
 from .virtual import IVMeasurerVirtual
 from .measurerivm import IVMeasurerIVM10
 from .measurerivm02 import IVMeasurerIVM02
@@ -13,11 +14,24 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", action="store", dest="port",
-                        help="Real IVM measurer COM port. Format: com:\\\\.\\COMx or /dev/ttyACM0")
+                        help="Real IVM measurer COM port. Format: com:\\\\.\\COMx or /dev/ttyACM0.\n"
+                             "Make sure there is 'config.ini' file in current working directory!")
+    parser.add_argument("-f", action="store", dest="firmware",
+                        help="First two numbers of firmware version. Format: x.x")
     args = parser.parse_args()
 
+    firmware_vs_handler = {"0.2": IVMeasurerIVM02,
+                           "1.0": IVMeasurerIVM10}
     if args.port is not None:
-        m = IVMeasurerIVM02(args.port, config="config.ini")
+        handler = firmware_vs_handler.get(args.firmware, None)
+        if handler is None:
+            supported_vers = ", ".join(firmware_vs_handler.keys())
+            raise BadFirmwareVersion("Please use '-f' argument to specify one of the supported firmware versions: "
+                                     "{}".format(supported_vers))
+        try:
+            m = handler(args.port, config="config.ini")
+        except (BadConfig, BadFirmwareVersion) as e:
+            raise type(e)("Something wrong with config file! Check example: 'epcore/ivmeasurer/config.ini'.")
     else:
         m = IVMeasurerVirtual()
 
