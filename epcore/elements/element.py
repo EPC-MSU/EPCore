824b5ca9cf118460dbcb7537945918fd43859076
Andrey Marakulin <devnull@localhost>
2021-07-27 13:52:30 +0300

    Fix set_automatically for tests.

--- epcore/elements/element.py
+++ epcore/elements/element.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import List, Optional, Dict, Tuple
 from .abstract import JsonConvertible
 from .pin import Pin
@@ -17,22 +17,33 @@ class Element(JsonConvertible):
     pins: List[Pin]
     name: Optional[str] = None
     package: Optional[str] = None
-    # center: Optional[Tuple[float, float]] = None
-    bounding_zone: Optional[List[Tuple[float, float]]] = None
+    center: Optional[Tuple[float, float]] = None
+    bounding_zone: Optional[List[Tuple[float, float]]] = field(default_factory=list)
     rotation: Optional[float] = None
     width: Optional[float] = None
     height: Optional[float] = None
-    set_automatically: Optional[bool] = False
+    set_automatically: Optional[bool] = None
 
-    @property
-    def center(self):
-        if len(self.bounding_zone) > 0:
-            return np.mean(np.array(self.bounding_zone), axis=0).tolist()
+    def __post_init__(self):
+        if self.bounding_zone is not None:
+            if len(self.bounding_zone) == 4 or len(self.bounding_zone) == 12:
+                self.center = np.mean(np.array(self.bounding_zone), axis=0).tolist()
         elif len(self.pins) > 0:
             arr_xy = [(p.x, p.y) for p in self.pins]
-            return np.mean(np.array(arr_xy), axis=0).tolist()
+            self.center = np.mean(np.array(arr_xy), axis=0).tolist()
         else:
-            return None
+            self.center = None
+
+    # @property
+    # def center(self):
+    #     if self.bounding_zone is not None:
+    #         if len(self.bounding_zone) > 0:
+    #             return np.mean(np.array(self.bounding_zone), axis=0).tolist()
+    #     elif len(self.pins) > 0:
+    #         arr_xy = [(p.x, p.y) for p in self.pins]
+    #         return np.mean(np.array(arr_xy), axis=0).tolist()
+    #     else:
+    #         return None
 
     def to_json(self) -> Dict:
         """
@@ -42,11 +53,12 @@ class Element(JsonConvertible):
 
         json_data = {
             "name": self.name,
-            "set_automatically" : self.set_automatically,
+            "set_automatically": self.set_automatically,
             "pins": [pin.to_json() for pin in self.pins],
             "package": self.package,
-            "center": self.center,
-            "bounding_zone": tuple(tuple(map(float, p)) for p in self.bounding_zone),
+            # "center": self.center,
+            "bounding_zone": tuple(tuple(map(float, p)) for p in self.bounding_zone)
+            if self.bounding_zone is not None else None,
             "rotation": self.rotation,
             "width": self.width,
             "height": self.height
