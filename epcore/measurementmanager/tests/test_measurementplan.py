89aca8ae9cdaa892d9002105331913678c7f32b2
timerke <timerke@mail.ru>
2024-01-31 17:47:00 +0300

    Fixed tests for measurement plan

--- epcore/measurementmanager/tests/test_measurementplan.py
+++ epcore/measurementmanager/tests/test_measurementplan.py
@@ -49,6 +49,18 @@ class TestPlan(unittest.TestCase):
         with self.assertRaises(ValueError):
             self._plan.go_pin(5)
 
+    def test_get_pins_without_multiplexer_outputs(self) -> None:
+        pins = [Pin(x=0, y=0, measurements=[]),
+                Pin(x=1, y=1, measurements=[], multiplexer_output=MultiplexerOutput(channel_number=3, module_number=2)),
+                Pin(x=2, y=2, measurements=[], multiplexer_output=MultiplexerOutput(channel_number=6, module_number=5))]
+        board = Board(elements=[Element(pins=pins)])
+        measurer = IVMeasurerVirtual()
+        multiplexer = AnalogMultiplexerVirtual()
+        plan = MeasurementPlan(board=board, measurer=measurer, multiplexer=multiplexer)
+
+        self.assertEqual(len(plan.elements[0].pins), 3)
+        self.assertEqual(plan.get_pins_without_multiplexer_outputs(), [0, 2])
+
     def test_go_prev_pin(self) -> None:
         self._plan.go_prev_pin()
         self.assertEqual(self._plan.get_current_pin(), self._pins[1])
@@ -91,31 +103,3 @@ class TestPlan(unittest.TestCase):
         self._plan.save_last_measurement_as_test()
         self.assertEqual(len(self._plan.get_current_pin().measurements), 1)
         self.assertFalse(self._plan.get_current_pin().measurements[0].is_reference)
-
-    def test_plan(self) -> None:
-        pins = [Pin(x=0, y=0, measurements=[]), Pin(x=1, y=1, measurements=[])]
-        board = Board(elements=[Element(pins=pins)])
-        measurer = IVMeasurerVirtual()
-        plan = MeasurementPlan(board=board, measurer=measurer)
-
-        # Add new pin and check that there are 3 pins
-        plan.append_pin(Pin(5, 5, measurements=[]))
-        self.assertTrue(len(plan.elements[0].pins) == 3)
-        # Trigger measure and save it as test
-        measurer.measure_iv_curve()
-        plan.save_last_measurement_as_test()
-        # Check that current pin has one measurement and this measurement is reference IVC
-        self.assertTrue(len(plan.get_current_pin().measurements) == 1)
-        self.assertFalse(plan.get_current_pin().measurements[0].is_reference)
-
-    def test_plan_with_multiplexer(self):
-        pins = [Pin(x=0, y=0, measurements=[]),
-                Pin(x=1, y=1, measurements=[], multiplexer_output=MultiplexerOutput(channel_number=3, module_number=2)),
-                Pin(x=2, y=2, measurements=[], multiplexer_output=MultiplexerOutput(channel_number=6, module_number=5))]
-        board = Board(elements=[Element(pins=pins)])
-        measurer = IVMeasurerVirtual()
-        multiplexer = AnalogMultiplexerVirtual()
-        plan = MeasurementPlan(board=board, measurer=measurer, multiplexer=multiplexer)
-
-        self.assertEqual(len(plan.elements[0].pins), 3)
-        self.assertEqual(plan.get_pins_without_multiplexer_outputs(), [0, 2])
