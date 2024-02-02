d7ab27b29699b8417ca95c053e430e2e09be10df
timerke <timerke@mail.ru>
2024-02-02 12:57:42 +0300

    Replaced scripts

--- scripts/testall.bat
+++ scripts/testall.bat
@@ -1,15 +1,15 @@
-@echo off
+cd ..
 setlocal EnableDelayedExpansion
+
+echo --- Install requirements ---
 if exist venv  rd /S /Q venv
 python -m venv venv
 venv\Scripts\python -m pip install --upgrade pip
-venv\Scripts\python -m pip install -r requirements.txt --upgrade
+venv\Scripts\python -m pip install -r requirements.txt
 venv\Scripts\python -m pip install flake8
+
 echo --- Run all tests ---
-for /D %%f in (epcore\*) do (
-echo Test %%f
-venv\Scripts\python _test.py %%f
-)
+venv\Scripts\python -m unittest discover -v
 
 echo --- Check flake8 ---
 venv\Scripts\python -m flake8 .
--- scripts/testall.sh
+++ scripts/testall.sh
@@ -1,19 +1,15 @@
+cd..
 rm -rf venv
 set -e
 
+echo "--- Install requirements ---"
 python3 -m venv venv
 ./venv/bin/python -m pip install --upgrade pip
 ./venv/bin/python -m pip install -r requirements.txt
 ./venv/bin/python -m pip install flake8
 
 echo "--- Run all tests ---"
-export PYTHONPATH=$(dirname "$0")
-for f in epcore/*/
-do
-dir=${f%*/}
-echo "Test $dir"
-./venv/bin/python _test.py "$dir"
-done;
+./venv/bin/python -m unittest discover -v
 
 echo "--- Check flake8 ---"
 ./venv/bin/python -m flake8 .
