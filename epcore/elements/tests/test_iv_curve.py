9548cf7be15959aa0b13f84557198c3379df3842
timerke <timerke@mail.ru>
2024-03-11 16:07:20 +0300

    Fixed typo

--- epcore/elements/tests/test_iv_curve.py
+++ epcore/elements/tests/test_iv_curve.py
@@ -5,15 +5,15 @@ from epcore.elements import IVCurve
 
 class IVCurveTests(unittest.TestCase):
 
-    def test_default_arrays(self):
+    def test_default_arrays(self) -> None:
         test_curve = IVCurve()
         self.assertTrue(len(test_curve.voltages))
         self.assertTrue(np.allclose(test_curve.voltages, [0, 0]))
 
-    def test_length_mismatch(self):
-        with self.assertRaises(ValueError):
-            IVCurve(currents=[1, 2, 3], voltages=[1, 2])
-
-    def test_incorrent_length(self):
+    def test_incorrect_length(self) -> None:
         with self.assertRaises(ValueError):
             IVCurve(currents=[1], voltages=[1])
+
+    def test_length_mismatch(self) -> None:
+        with self.assertRaises(ValueError):
+            IVCurve(currents=[1, 2, 3], voltages=[1, 2])
