0a5a0af534a1e7244dfc504e821e6d962a7cbb24
timerke <timerke@mail.ru>
2024-03-11 16:04:25 +0300

    Changed to_json method of Board

--- epcore/elements/board.py
+++ epcore/elements/board.py
@@ -1,3 +1,4 @@
+import os
 from dataclasses import dataclass, field
 from typing import Any, Dict, List, Optional, Union
 from warnings import warn
@@ -23,7 +24,7 @@ class PCBInfo(JsonConvertible):
     def create_from_json(cls, json_data: Dict[str, Any]) -> "PCBInfo":
         """
         Create object from dict with structure compatible with UFIV JSON file schema.
-        :param json_data: dict with information.
+        :param json_data: dictionary with information.
         :return: object.
         """
 
@@ -35,8 +36,7 @@ class PCBInfo(JsonConvertible):
 
     def to_json(self) -> Dict[str, Union[float, str]]:
         """
-        Return dict with structure compatible with UFIV JSON file schema.
-        :return: dict with information about object.
+        :return: dictionary with information about object with structure compatible with UFIV JSON file schema.
         """
 
         json_data = {"pcb_name": self.pcb_name,
@@ -58,23 +58,35 @@ class Board(JsonConvertible):
     @classmethod
     def create_from_json(cls, json_data: Dict[str, Any]) -> "Board":
         """
-        Create object from dict with structure compatible with UFIV JSON file schema.
-        :param json_data: dict with information about board.
+        Create object from dictionary with structure compatible with UFIV JSON file schema.
+        :param json_data: dictionary with information about board.
         :return: board object.
         """
 
         if json_data["version"] != version:
             warn(f"Module version {version} does not match version of file {json_data['version']}")
         return Board(
-            elements=[Element.create_from_json(el) for el in json_data["elements"]],
+            elements=[Element.create_from_json(element) for element in json_data["elements"]],
             pcb=PCBInfo.create_from_json(json_data.get("PCB", {}))
         )
 
-    def to_json(self) -> Dict[str, Any]:
+    def to_json(self, save_image_if_needed_to: Optional[str] = None, board_path: Optional[str] = None
+                ) -> Dict[str, Any]:
         """
-        Return dict with structure compatible with UFIV JSON file schema.
-        :return: dict with information about board.
+        :param save_image_if_needed_to: folder where to save the image, if the board has an image;
+        :param board_path: path to the file where the board is saved.
+        :return: dictionary with information about board with structure compatible with UFIV JSON file schema.
         """
 
-        return {"elements": [el.to_json() for el in self.elements],
+        pcb_info = self.pcb.to_json() if self.pcb is not None else dict()
+        if save_image_if_needed_to and board_path and self.image:
+            os.makedirs(save_image_if_needed_to, exist_ok=True)
+            image_path = os.path.join(save_image_if_needed_to, "image.png")
+            self.image.save(image_path)
+            pcb_info["pcb_image_path"] = os.path.relpath(image_path, os.path.dirname(board_path))
+
+        data = {"elements": [element.to_json() for element in self.elements],
                 "version": version}
+        if pcb_info:
+            data["PCB"] = pcb_info
+        return data
