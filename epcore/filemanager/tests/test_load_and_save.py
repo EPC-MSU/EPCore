052c2b66d266978e50180906f2ff4b6e37d2aa9e
timerke <timerke@mail.ru>
2024-03-20 15:29:01 +0300

    Changed tests for filemanager

--- epcore/filemanager/tests/test_load_and_save.py
+++ epcore/filemanager/tests/test_load_and_save.py
@@ -8,48 +8,55 @@ from epcore.elements import Board
 from epcore.filemanager import load_board_from_ufiv, save_board_to_ufiv
 
 
-board_path = os.path.join(os.path.dirname(__file__), "testboard.json")
-invalid_board_path = os.path.join(os.path.dirname(__file__), "testboard_invalid.json")
-oldstyle_board_path = os.path.join(os.path.dirname(__file__), "oldstyleboard.json")
-invalid_oldstyle_board_path = os.path.join(os.path.dirname(__file__), "oldstyleboard_invalid.json")
-dummy_path = os.path.join(os.path.dirname(__file__), "no_such_file.json")
-image_path = os.path.join(os.path.dirname(__file__), "testboard.png")
-oldstyle_board_with_img_path = os.path.join(os.path.dirname(__file__), "testboard_with_img",
-                                            "board.json")
-oldstyle_board_without_img_path = os.path.join(os.path.dirname(__file__), "testboard_without_img",
-                                               "board.json")
-
-
 class LoadSaveTests(unittest.TestCase):
 
-    def test_path_not_exists(self):
+    dir_path: str = None
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.dir_path: str = os.path.join(os.path.dirname(__file__), "data")
+
+    def test_load_board_from_ufiv_format_ufiv(self) -> None:
+        board_path = os.path.join(LoadSaveTests.dir_path, "ufiv_format", "testboard.json")
+        board = load_board_from_ufiv(board_path)
+        self.assertIsNotNone(board.image)
+        self.assertEqual(board.image.width, 100)
+        self.assertEqual(board.image.height, 100)
+
+    def test_load_board_from_ufiv_old_style_format(self) -> None:
+        old_style_board_path = os.path.join(LoadSaveTests.dir_path, "old_style_board.json")
+        board = load_board_from_ufiv(old_style_board_path, auto_convert_p10=True)
+        self.assertTrue(bool(board))
+
+    def test_load_board_from_ufiv_old_style_format_validation(self):
+        invalid_old_style_board_path = os.path.join(LoadSaveTests.dir_path, "old_style_board_invalid.json")
+        with self.assertRaises(ValidationError):
+            load_board_from_ufiv(invalid_old_style_board_path, auto_convert_p10=True, validate_input=True)
+
+    def test_load_board_from_ufiv_old_style_board_with_image(self) -> None:
+        old_style_board_with_img_path = os.path.join(LoadSaveTests.dir_path, "testboard_with_img", "board.json")
+        board = load_board_from_ufiv(old_style_board_with_img_path)
+        self.assertTrue(bool(board.image))
+
+    def test_load_board_from_ufov_old_style_board_without_image(self) -> None:
+        old_style_board_without_img_path = os.path.join(LoadSaveTests.dir_path, "testboard_without_img", "board.json")
+        board = load_board_from_ufiv(old_style_board_without_img_path)
+        self.assertFalse(bool(board.image))
+
+    def test_load_board_from_ufiv_path_not_exists(self) -> None:
+        dummy_path = os.path.join(LoadSaveTests.dir_path, "no_such_file.json")
         with self.assertRaises(FileNotFoundError):
             load_board_from_ufiv(dummy_path)
 
-    def test_load_board(self):
-        board = load_board_from_ufiv(board_path)
-        self.assertTrue(board.image is not None)
-        self.assertTrue(board.image.width == 100)
-        self.assertTrue(board.image.height == 100)
-
-    def test_oldstyle_load(self):
-        board = load_board_from_ufiv(oldstyle_board_path, auto_convert_p10=True)
-        self.assertTrue(bool(board))
-
-    def test_load_validation(self):
-        # Bad board should cause an exception
+    def test_load_board_from_ufiv_validation(self) -> None:
+        invalid_board_path = os.path.join(LoadSaveTests.dir_path, "test_board_invalid.json")
         with self.assertRaises(ValidationError):
             load_board_from_ufiv(invalid_board_path, validate_input=True)
 
-    def test_oldstyle_load_validation(self):
-        # Bad board should cause an exception
-        with self.assertRaises(ValidationError):
-            load_board_from_ufiv(invalid_oldstyle_board_path, auto_convert_p10=True,
-                                 validate_input=True)
-
-    def test_save_board(self):
-        board = Board([])
-        board.image = Image.open(image_path)
+    def test_save_board_to_ufiv(self) -> None:
+        image_path = os.path.join(LoadSaveTests.dir_path, "ufiv_format", "testboard.png")
+        image = Image.open(image_path)
+        board = Board(elements=[], image=image)
 
         with TemporaryDirectory() as tempdir:
             save_board_to_ufiv(os.path.join(tempdir, "foo.uzf"), board)
@@ -59,11 +66,3 @@ class LoadSaveTests(unittest.TestCase):
             files = archive.namelist()
             self.assertTrue("foo.json" in files)
             self.assertTrue("foo.png" in files)
-
-    def test_load_old_style_board_with_image(self):
-        board = load_board_from_ufiv(oldstyle_board_with_img_path)
-        self.assertTrue(bool(board.image))
-
-    def test_load_old_style_board_without_image(self):
-        board = load_board_from_ufiv(oldstyle_board_without_img_path)
-        self.assertFalse(bool(board.image))
