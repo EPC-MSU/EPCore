36b0609fc975615467c7d10fef0b40cbd2c112d0
timerke <timerke@mail.ru>
2024-03-28 13:26:40 +0300

    Vesrion incremenet

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 
 setup(name="epcore",
-      version="0.1.22",
+      version="0.1.23",
       description="A common set of key modules on the basis of which various software is built that works with "
                   "EyePoint signature analyzers",
       url="https://gitlab.ximc.ru/eyepoint/epcore",
