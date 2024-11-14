536e58104d36d5a069fde230c5b30e7acaf5168a
timerke <timerke@mail.ru>
2024-11-14 15:58:13 +0300

    Removed ivm02

--- epcore/ivmeasurer/__init__.py
+++ epcore/ivmeasurer/__init__.py
@@ -24,10 +24,10 @@ pydoc -w epcore.ivmeasurer
 
 from .base import IVMeasurerBase, IVMeasurerIdentityInformation
 from .measurerasa import IVMeasurerASA, IVMeasurerVirtualASA
-from .measurerivm import IVMeasurerIVM02, IVMeasurerIVM10
+from .measurerivm import IVMeasurerIVM10
 from .virtual import IVMeasurerVirtual
 from .virtualbad import IVMeasurerVirtualBad
 
 
-__all__ = ["IVMeasurerASA", "IVMeasurerBase", "IVMeasurerIdentityInformation", "IVMeasurerIVM02", "IVMeasurerIVM10",
-           "IVMeasurerVirtual", "IVMeasurerVirtualASA", "IVMeasurerVirtualBad"]
+__all__ = ["IVMeasurerASA", "IVMeasurerBase", "IVMeasurerIdentityInformation", "IVMeasurerIVM10", "IVMeasurerVirtual",
+           "IVMeasurerVirtualASA", "IVMeasurerVirtualBad"]
