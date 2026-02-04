7fbe5290ebd570f4dd9b67a0d41371d4b35fe192
timerke <timerke@mail.ru>
2026-02-04 17:03:39 +0300

    Removed loading lib for asa in Linux because we did not build libs for Astra Linux #122577

--- epcore/ivmeasurer/asa10/asa.py
+++ epcore/ivmeasurer/asa10/asa.py
@@ -214,13 +214,8 @@ def _get_dll() -> Optional[CDLL]:
     """
 
     if platform.system() == "Linux":
-        if platform.uname()[4] == "aarch64":
-            # Library for this device was not built
-            return
-        libraries = [_get_full_path(os.path.join("libasa-debian", library))
-                     for library in ADDITIONAL_LIBRARIES_FOR_LINUX]
-        _load_additional_libraries(libraries)
-        return CDLL(_get_full_path(os.path.join("libasa-debian", "libasa.so")))
+        return
+
     if system() == "Windows":
         if 8 * struct.calcsize("P") == 32:
             libraries = [_get_full_path(os.path.join("libasa-win32", library))
@@ -231,6 +226,7 @@ def _get_dll() -> Optional[CDLL]:
                      for library in ADDITIONAL_LIBRARIES_FOR_WINDOWS]
         _load_additional_libraries(libraries)
         return CDLL(_get_full_path(os.path.join("libasa-win64", "asa.dll")))
+
     raise NotImplementedError("Unsupported platform {}".format(system()))
 
 
