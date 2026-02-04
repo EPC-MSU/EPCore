42293a048b8af09b5e728df26b118dd60d1fca9f
timerke <timerke@mail.ru>
2026-02-04 16:53:45 +0300

    Changed libepmux.so for Astra Linux #122577

--- scripts/testall.sh
+++ scripts/testall.sh
@@ -3,7 +3,7 @@ rm -rf venv
 set -e
 
 echo "--- Install requirements ---"
-python3 -m venv venv
+python3.6 -m venv venv
 ./venv/bin/python -m pip install --upgrade pip
 ./venv/bin/python -m pip install -r requirements.txt
 ./venv/bin/python -m pip install flake8
